c55ea002c50738229b290143567482a9d5cb8a9a
alpefe44 <efe1998alp@hotmail.com>
2024-06-27 14:15:45 +0300

    .

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import HomeScreen from './src/Screens/HomeScreen';
+import { StoryProvider } from './src/Context/StoryData';
+import LoginScreen from './src/Screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/Router/Router';
+import { UserProvider } from './src/Context/UserContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={style.AndroidSafeArea}>
+      <UserProvider>
+        <StoryProvider>
+          <NavigationContainer>
+            <Router></Router>
+          </NavigationContainer>
+        </StoryProvider>
+      </UserProvider>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({
+  AndroidSafeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
